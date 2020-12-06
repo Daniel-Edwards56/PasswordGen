4ec01d31374b010c018759046005beac153337f4
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2020-12-06 15:34:30 -0500

    Added comments explaining every step of the js code

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
+// This is and empty string to store object choices.
 var randomChar = "";
+// An object to house the charecters, title of choices and boolean of the choices.
 var charObj = [
   {
     char: "!@#$%^&*()_+-=\/?,.{}[]",
@@ -37,15 +24,13 @@ var charObj = [
   },
 ]
 
-
-
-
-
-
-
+// Main function that's called on onClick when hitting Generate Password on the app page.
 var generateBtn = () => {
+  //Prompt for to pick between password legnth of 8 to 128, if outside bounds sends an error.
 var passwordLength = parseInt(prompt("Please enter a number between 8 and 128"));
+// Empty variable string to store value for getElementByID
 var passwordString = "";
+//If within bounds, prompts user to select categories and store responses
  if (passwordLength >= 8 && passwordLength <= 128){
    charObj.forEach(set => {
      var useChar = (prompt(`Would you like to include ${set.category} type yes or no`)).toUpperCase();
@@ -60,14 +45,19 @@ var passwordString = "";
 else {
   alert("Hey let's try that again looks like something went wrong")
 }
+//This is a for loop to iterate through the length of the password and sets each character to a random value of user choice.
 if(randomChar !== ""){
   for(i=1; i<=passwordLength; i++){
     passwordString = passwordString + randomChar.charAt(Math.floor(Math.random() * Math.floor((randomChar.length)-1)))
   }
-  document.getElementById("password").value = passwordString
+  //This assigns the generated password to the placeholder string set in the text area
+  document.getElementById("password").value = passwordString;
+ 
 }
+//This is the error if the user prompts no to every category
 else{
   alert ("Please select an input type")
 }
+
 }
 
