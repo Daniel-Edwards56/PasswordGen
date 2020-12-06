3914a7750f97dc0c0e2c0c8e7b732f9b321227be
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2020-12-06 15:09:11 -0500

    made it so the the password string appears on the html where the placeholder text is having, trouble running the app twice before refresh

--- script.js
+++ script.js
@@ -45,7 +45,29 @@ var charObj = [
 
 var generateBtn = () => {
 var passwordLength = parseInt(prompt("Please enter a number between 8 and 128"));
-var paswwordString = "";
- if
-
+var passwordString = "";
+ if (passwordLength >= 8 && passwordLength <= 128){
+   charObj.forEach(set => {
+     var useChar = (prompt(`Would you like to include ${set.category} type yes or no`)).toUpperCase();
+    if (useChar === "YES"){
+      set.choice = true;
+    }
+    if (set.choice){
+      randomChar = randomChar + set.char;
+    }
+   })
+ }
+else {
+  alert("Hey let's try that again looks like something went wrong")
 }
+if(randomChar !== ""){
+  for(i=1; i<=passwordLength; i++){
+    passwordString = passwordString + randomChar.charAt(Math.floor(Math.random() * Math.floor((randomChar.length)-1)))
+  }
+  document.getElementById("password").value = passwordString
+}
+else{
+  alert ("Please select an input type")
+}
+}
+
